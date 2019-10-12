1dd8ab3a2db8d3a0e8ad0a2ec698f4b30ffc6803
Liliia Kuzyeva <gilmirahman@gmail.com>
2019-10-12 15:28:13 +0500

    Задание 26.  Исправление. В тестах на добаление и удаление проекта реализована загрузка списка проектов через SOAP

--- test/test_del_project.py
+++ test/test_del_project.py
@@ -7,9 +7,9 @@ def test_add_project(app):
     password = 'root'
     if app.project.count() == 0:
         app.project.create(Project(name="New project"))
-    old_projects = app.project.get_project_list()
+    old_projects = app.soap.get_project_list(username, password)
     project = random.choice(old_projects)
     app.project.delete_project(project.id)
-    new_projects = app.project.get_project_list()
-    soap_projects = app.soap.get_project_list(username, password)
-    assert sorted(soap_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
+    new_projects = app.soap.get_project_list(username, password)
+    old_projects.remove(project)
+    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
